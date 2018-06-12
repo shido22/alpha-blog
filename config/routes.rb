35083a93cebc29f5f842efb5d6e7811cc55eabac
shido22 <22stealth@gmail.com>
2018-06-12 11:36:53 -0400

    Create Homepage and Add links

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   
-  root 'pages#home'
-  get 'about', to: 'pages#about'
+  root 'articles#home'
+  get 'about', to: 'articles#about'
   
   resources :articles
 end
